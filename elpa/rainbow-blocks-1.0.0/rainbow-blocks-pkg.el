63c27b92e5da28e890cbb8ca731597430ee135b6
Juraj Hreško <juraj.hresko@firma.seznam.cz>
2016-02-02 19:13:59 +0100

    Initial commit

---
+++ elpa/rainbow-blocks-1.0.0/rainbow-blocks-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rainbow-blocks" "1.0.0" "Block syntax highlighting for lisp code" 'nil :url "https://github.com/istib/rainbow-blocks")
---
+++ elpa/rainbow-identifiers-0.2.2/rainbow-identifiers-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rainbow-identifiers" "0.2.2" "Highlight identifiers according to their names" '((emacs "24")) :url "https://github.com/Fanael/rainbow-identifiers")
